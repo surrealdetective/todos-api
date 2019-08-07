58dc21c7301692ced0f0bbf01999f5d82ea14050
surrealdetective <thefifth@gmail.com>
2019-08-07 10:24:35 -0700

    Support api versions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  # ToDo Domain
-  resources :todos do
-    resources :items
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :todos do
+      resources :items
+    end
   end
 
-  # Auth
+
   post 'auth/login', to: 'authentication#authenticate'
   post 'signup', to: 'users#create'
 end
