89f6e9a4a163b8ecbbedb4bbd2d1471b53f37043
surrealdetective <thefifth@gmail.com>
2019-08-07 10:56:29 -0700

    Set no-cache default for api, adjust per controller action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::API
 
   # called before every action on controllers
   before_action :authorize_request
+  before_action :set_cache_headers #https://stackoverflow.com/questions/45329731/csrf-tokens-to-not-match-what-is-in-session-rails-4-1 #https://devcenter.heroku.com/articles/http-caching-ruby-rails
   attr_reader :current_user
 
   private
@@ -11,5 +12,11 @@ class ApplicationController < ActionController::API
   # Check for valid request token and return user
   def authorize_request
     @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
+
+  def set_cache_headers
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "Mon, 01 Jan 1990 00:00:00 GMT"
   end
 end
